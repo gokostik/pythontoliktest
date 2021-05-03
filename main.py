a9b7b0efd55e3f95b2b148d5166794d9ae805d20
gokostik <gokostik@ukr.net>
2021-05-03 20:33:21 +0300

    5555

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-age = 0
-print("привіт хресний")
-age = input("скільки тобі років?")
-print(f"о... {age}... то ти старий пердун...")
+#print("привіт хресний")
+#age = input("скільки тобі років?")
+#print(f"о... {age}... то ти старий пердун...")
+math = True
+is_a_cool = False
+is_b_cool = False
+
+while math:
+
+
+# ВВЕДЕННЯ А
+    while not is_a_cool:
+        try:
+            A = float(input("введіть a=").replace(",","."))
+            is_a_cool = True
+        except:
+            print("a має бути числом")
+
+# ВВЕДЕННЯ Б
+    while not is_b_cool:
+        try:
+            B = float(input("введіть B=").replace(",","."))
+            is_b_cool = True
+        except:
+            print("B має бути числом")
+
+
+    print(f"A-B={A-B}")
+    print(f"A+B={A+B}")
+    print(f"A*B={A*B}")
+    try:
+        print(f"A/B={A/B}")
+    except:
+        print("ділити на нуль не можна")
+
+
+    is_a_cool = False
+    is_b_cool = False
