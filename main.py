5f46b5669cb55faef7bb0095e6343bf7fd52b954
gokostik <gokostik@ukr.net>
2021-05-03 20:46:58 +0300

    JOPA

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ while math:
 # ВВЕДЕННЯ А
     while not is_a_cool:
         try:
-            A = float(input("введіть a=").replace(",","."))
+            A = input("введіть a=").replace(",",".")
+            A = float(A)
             is_a_cool = True
         except:
+            if A == "exit":
+                quit(0)
             print("a має бути числом")
 
 # ВВЕДЕННЯ Б
@@ -24,15 +27,14 @@ while math:
         except:
             print("B має бути числом")
 
-
-    print(f"A-B={A-B}")
-    print(f"A+B={A+B}")
-    print(f"A*B={A*B}")
-    try:
-        print(f"A/B={A/B}")
-    except:
-        print("ділити на нуль не можна")
-
+    if math:
+        print(f"A-B={A-B}")
+        print(f"A+B={A+B}")
+        print(f"A*B={A*B}")
+        try:
+            print(f"A/B={A/B}")
+        except:
+            print("ділити на нуль не можна")
 
     is_a_cool = False
     is_b_cool = False
